2254c6cfce64e56da73dde0aba8db38d27d7de2c
jotaulian <juluegglepalomo@gmail.com>
2022-01-05 10:11:11 +0100

    Calculator app with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  title = 'Aplicación de Calculadora';
+  operandA: number = 0;
+  operandB: number = 0;
+  resultado: number = 0;
+
+  addNumbers(): void{
+    this.resultado = this.operandA + this.operandB;
+  }
+  clear():void {
+     this.operandA = 0;
+     this.operandB = 0;
+     this.addNumbers();
+  }
 }
